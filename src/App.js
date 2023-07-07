7037e67c01248db2826faceb1b7cb14a232fc900
bvenceslas <bvenceslas@gmail.com>
2023-07-07 09:23:23 +0200

    Make layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
-function App() {
-  return <div>Hello React!</div>;
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip ?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description} <button>❌</button>
+      </span>
+    </li>
+  );
+};
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 you have X elements on your list, and you have packed X (X%)</em>
+    </footer>
+  );
+}
